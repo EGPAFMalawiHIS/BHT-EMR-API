60c799f35898c96b81bff927fd997e58e8cc6734
Roy Chanunkha <roychanunkha@gmail.com>
2024-11-14 15:38:22 +0200

    chore: encoding the site data

--- app/controllers/api/v1/qr_code_controller.rb
+++ app/controllers/api/v1/qr_code_controller.rb
@@ -10,11 +10,26 @@ module Api
       skip_before_action :check_location
 
       def site_qr_code
+        base64 = params[:base64]&.casecmp?('true')
         png = png_generator data: site_data
-        send_data png.to_s, type: 'image/png', disposition: 'inline'
+        if base64
+          render json: { qr_code: "data:image/png;base64,#{Base64.encode64(png.to_s)}" }, status: :ok
+        else
+          send_data png.to_s, type: 'image/png', disposition: 'inline'
+        end
       end
 
       private
+
+      def encrypt_data(data)
+        pass_key = YAML.load_file("#{Rails.root}/config/application.yml", aliases: true)[Rails.env]['qr_code_key']
+        hashed_key = Digest::SHA256.digest(pass_key)
+        cipher = OpenSSL::Cipher.new('AES-256-CBC')
+        cipher.encrypt # set the cipher to be encryption mode
+        cipher.key = hashed_key # set the key
+        encrypted = cipher.update(data) + cipher.final
+        Base64.strict_encode64(encrypted)
+      end
 
       ## The code should be moved to a service if the needs start to grow extremely
       def site_data
@@ -28,7 +43,7 @@ module Api
 
 
       def png_generator(data:)
-        qr = RQRCode::QRCode.new(data.to_json)
+        qr = RQRCode::QRCode.new(encrypt_data(data.to_json))
         qr.as_png(
           bit_depth: 1,
           border_modules: 4,
