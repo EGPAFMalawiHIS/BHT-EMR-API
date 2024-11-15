9559c66935a5e71c46ed7116a6de3d090afa9498
Roy Chanunkha <roychanunkha@gmail.com>
2024-11-15 08:47:03 +0200

    chore: tests

--- bin/test_order.rb
+++ bin/test_order.rb
@@ -87,7 +87,7 @@ def get_tests(specimens)
     specimen = specimens.sample
   end
 
-  sample_size = tests.size > 2 ? rand(1..tests.size) : 1
+  sample_size = tests.size > 4 ? 3 : 1
 
   {specimen:, tests: tests.sample(sample_size)}
 end
