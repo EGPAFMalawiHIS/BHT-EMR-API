b780b6f07e9ff87d08f1f4fc2c0608ee5f9ffc2f
Brian M <brianmsyamboza@gmail.com>
2025-01-06 14:05:39 +0200

    update script to handle configurations with primary and secondary configs

--- bin/update_art_metadata.sh
+++ bin/update_art_metadata.sh
@@ -18,19 +18,27 @@ set -x # turns on stacktrace mode which gives useful debug information
 export RAILS_ENV=$ENV
 rails db:environment:set RAILS_ENV=$ENV
 
-USERNAME=`ruby -ryaml -e "puts YAML.safe_load(File.read('config/database.yml'), aliases: true)['${ENV}']['username']"`
-PASSWORD=`ruby -ryaml -e "puts YAML.safe_load(File.read('config/database.yml'), aliases: true)['${ENV}']['password']"`
-DATABASE=`ruby -ryaml -e "puts YAML.safe_load(File.read('config/database.yml'), aliases: true)['${ENV}']['database']"`
-HOST=`ruby -ryaml -e "puts YAML.safe_load(File.read('config/database.yml'), aliases: true)['${ENV}']['host']"`
-PORT=`ruby -ryaml -e "puts YAML.safe_load(File.read('config/database.yml'), aliases: true)['${ENV}']['port']"`
+set +x # turns off stacktrace mode
+# Load database configuration using Rails
+CONFIG=$(rails runner "require 'yaml'; require 'erb'; puts YAML.safe_load(ERB.new(File.read('config/database.yml')).result, aliases: true).to_json")
+
+DB_CONFIG=$(echo $CONFIG | jq -r --arg env "$ENV" '.[$env]["primary"] // .[$env] // empty')
+
+USERNAME=$(echo $DB_CONFIG | jq -r '.username')
+PASSWORD=$(echo $DB_CONFIG | jq -r '.password')
+DATABASE=$(echo $DB_CONFIG | jq -r '.database')
+HOST=$(echo $DB_CONFIG | jq -r '.host')
+PORT=$(echo $DB_CONFIG | jq -r '.port')
 
 # Only update metadata if migration is successful
 rails db:migrate && {
-  mysql --host=$HOST --port=$PORT --user=$USERNAME --password=$PASSWORD $DATABASE < db/sql/openmrs_metadata_1_7.sql
-  mysql --host=$HOST --port=$PORT --user=$USERNAME --password=$PASSWORD $DATABASE < db/sql/bart2_views_schema_additions.sql
-  mysql --host=$HOST --port=$PORT --user=$USERNAME --password=$PASSWORD $DATABASE < db/initial_setup/anc2_schema_additions.sql
-  mysql --host=$HOST --port=$PORT --user=$USERNAME --password=$PASSWORD $DATABASE < db/sql/moh_regimens_v2021.sql
-  mysql --host=$HOST --port=$PORT --user=$USERNAME --password=$PASSWORD $DATABASE < db/sql/drug_cms_metadata.sql
-  mysql --host=$HOST --port=$PORT --user=$USERNAME --password=$PASSWORD $DATABASE < db/sql/ntp_regimens.sql
+  for sql_file in db/sql/openmrs_metadata_1_7.sql \
+                  db/sql/bart2_views_schema_additions.sql \
+                  db/initial_setup/anc2_schema_additions.sql \
+                  db/sql/moh_regimens_v2021.sql \
+                  db/sql/drug_cms_metadata.sql \
+                  db/sql/ntp_regimens.sql; do
+    mysql --host=$HOST --port=$PORT --user=$USERNAME --password=$PASSWORD $DATABASE < $sql_file
+  done
 }
 
