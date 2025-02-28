097fbc5d95ca6a8a6d7ed1895251889c0362b76f
Daniel Justin <danieljmuleke@gmail.com>
2025-02-28 12:19:27 +0200

    Fix: ambigous site_id

--- app/services/opd_service/report_engine.rb
+++ app/services/opd_service/report_engine.rb
@@ -65,10 +65,10 @@ module OpdService
     def with_nids(start_date, end_date)
       type = PatientIdentifierType.find_by_name 'Malawi National ID'
 
-      data = Person.where('identifier_type = ? AND identifier != ? AND identifier != ? AND identifier != ? AND site_id = ?', type.id,
+      data = Person.where('identifier_type = ? AND identifier != ? AND identifier != ? AND identifier != ? AND n.site_id = ?', type.id,
                           'unknown', 'N/A', '', Location.current.location_id)\
                    .joins('INNER JOIN patient_identifier i ON i.patient_id = person.person_id
-        RIGHT JOIN person_address a ON a.person_id = person.person_id
+        RIGHT JOIN person_address a ON a.person_id = person.person_id AND a.site_id = person.site_id
         RIGHT JOIN person_name n ON n.person_id = person.person_id')\
                    .where(n: { date_created: start_date..end_date })
                    .select('person.*, a.state_province district, i.identifier nid,
