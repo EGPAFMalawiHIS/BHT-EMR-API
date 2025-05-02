e65eec9b600f533e3d0b84a90c367e8d3c7715c9
fchiyenda <fchiyenda>
2025-05-02 14:05:44 +0200

    Working with old spine database now

--- bin/spine/adapt_spine_to_new_architecture.rb
+++ bin/spine/adapt_spine_to_new_architecture.rb
@@ -21,6 +21,26 @@ end
 
 connection.change_column :obs, :location_id, :integer, default: nil, null: true
 
+ActiveRecord::Base.connection.execute(<<~SQL)
+  CREATE TABLE IF NOT EXISTS `order_extension` (
+    `order_extension_id` int(11) NOT NULL AUTO_INCREMENT,
+    `order_id` int(11) NOT NULL,
+    `value` varchar(50) NOT NULL DEFAULT '',
+    `creator` int(11) NOT NULL DEFAULT '0',
+    `date_created` datetime NOT NULL DEFAULT '1900-01-01 00:00:00',
+    `voided` tinyint(1) NOT NULL DEFAULT '0',
+    `voided_by` int(11) DEFAULT NULL,
+    `date_voided` datetime DEFAULT NULL,
+    `void_reason` varchar(255) DEFAULT NULL,
+    PRIMARY KEY (`order_extension_id`),
+    KEY `user_who_created_ext` (`creator`),
+    KEY `user_who_retired_ext` (`voided_by`),
+    KEY `retired_status` (`voided`),
+    CONSTRAINT `user_who_created_extension` FOREIGN KEY (`creator`) REFERENCES `users` (`user_id`),
+    CONSTRAINT `user_who_voided_extension` FOREIGN KEY (`voided_by`) REFERENCES `users` (`user_id`)
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+SQL
+
 # Recreate Patient Identifier table
 ActiveRecord::Base.connection.execute(<<~SQL)
   CREATE TABLE IF NOT EXISTS `patient_identifier_main` (
@@ -141,39 +161,23 @@ migrations_to_skip = %w[
   20110727100509
   20140221071909
   20140221071959
-  20140723080240
   20140728112036
-  20160112152154
   20160914173912
   20170309093939
   20171103064127
   20171114083901
-  20190219134117
-  20190410120250
-  20190410120646
-  20190503064300
-  20190508125042
-  20190510124325
   20190515130847
-  20190516091445
   20190516134103
   20190523131939
-  20190527122235
   20190527130805
   20200624084028
   20200624084345
   20200624084431
-  20210210114015
-  20210318125442
   20210415173348
   20220403182327
-  20220420085001
   20220725075253
-  20220725095428
-  20221112075527
   20230309133251
   20230823071343
-  20190604094625
   20190604100015
   20190604113533
   20190612113802
@@ -182,13 +186,20 @@ migrations_to_skip = %w[
   20220105140130
   20220524083521
   20220913143642
-  20230404133238
   20240625064618
+  20181210122430 
 ]
 
 migrations_to_skip.each { |migration| ActiveRecord::SchemaMigration.find_or_create_by!(version: migration) }
 
-sql_files = %i[spine_adaptaion]
+migration = ActiveRecord::MigrationContext.new(
+  ActiveRecord::Migrator.migrations_paths,
+  ActiveRecord::SchemaMigration
+)
+
+migration.run(:up, 20250325071602)
+
+sql_files = %i[spine_adaptaion bypass_migrations]
 
 sql_files.each do |file|
   sql = File.read("db/sql/#{file}.sql")
@@ -200,6 +211,7 @@ sql_files.each do |file|
   end
 end
 
+puts 'Importing art updated meta-data'
 `./bin/update_art_metadata.sh #{Rails.env}`
 
 # Set site to queens
