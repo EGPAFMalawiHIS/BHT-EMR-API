e57980fa6ecad2db7aeaec74f89928a4b22073f0
brian@xps <brianmsyamboza@gmail.com>
2025-07-08 11:10:43 +0200

    fix migrations for new rails version

--- db/migrate/20180827062036_add_token_expiry_time_to_user.rb
+++ db/migrate/20180827062036_add_token_expiry_time_to_user.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
 class AddTokenExpiryTimeToUser < ActiveRecord::Migration[5.2]
-  def change
+  def up
     execute 'ALTER TABLE users MODIFY COLUMN date_created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP()'
     add_column :users, :token_expiry_time, :date
+  end
+
+  def down
+    remove_column :users, :token_expiry_time if column_exists?(:users, :token_expiry_time)
   end
 end
--- db/migrate/20190123131130_add_deactivated_on_to_users.rb
+++ db/migrate/20190123131130_add_deactivated_on_to_users.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 class AddDeactivatedOnToUsers < ActiveRecord::Migration[5.2]
-  def change
+  def up
     add_column :users, :deactivated_on, :datetime, default: nil unless column_exists?(:users, :deactivated_on)
+  end
+
+  def down
+    remove_column :users, :deactivated_on if column_exists?(:users, :deactivated_on)
   end
 end
